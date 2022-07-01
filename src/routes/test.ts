42b2a89cb69c04b8b08421dc681407107b49c9a5
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2022-07-01 21:47:50 +0300

    Initial commit
    Signed-off-by: Kaleab S. Melkie <kaleabmelkie@gmail.com>

---
+++ src/routes/test.ts
@@ -0,0 +1,15 @@
+import vm2 from "vm2";
+
+export async function get() {
+  console.log({ vm2 });
+
+  const nodeVm = new vm2.NodeVM();
+  nodeVm.run('console.log("log from inside the vm")');
+
+  return {
+    status: 200,
+    body: {
+      message: "Hi!",
+    },
+  };
+}
